a88679de73433174244b0ce30b3a23879f9fa604
Evgenii <11>
2020-10-11 21:24:10 +0300

    update: set resolver

--- src/index.js
+++ src/index.js
@@ -1,37 +1,73 @@
 const SUDOKU_SECTION_DIVIDER = 3;
+const SUDOKU_VALUES = Array.from(Array(9)).map((x, i) => i + 1);
 
 window.onload = () => {
-  // TODO removed
-  function toHtml(puzzle) {
-    const table = document.getElementById('sudoku');
-    puzzle.forEach(element => {
-      const tr = document.createElement('tr');
-      element.forEach(tdValue => {
-        const td = document.createElement('td');
-        td.innerText = tdValue;
-        tr.appendChild(td);
-      });
-      table.appendChild(tr);
-    });
-  }
-
-  function setValues(puzzle) {
-    return puzzle.every(line => line.every(Boolean)) ? puzzle : sudoku(puzzle);
-  }
-
   function sudoku(puzzle) {
-    toHtml(puzzle);
-    //return the solved puzzle as a 2d array of 9 x 9
-    return setValues(puzzle);
+    for (const lineIndex in puzzle) {
+      for (const columnIndex in puzzle[lineIndex]) {
+        puzzle[lineIndex][columnIndex] =
+          puzzle[lineIndex][columnIndex] || getProbableValue(puzzle, +lineIndex, +columnIndex);
+      }
+    }
+    return puzzle.some(line => line.includes(0)) ? sudoku(puzzle) : puzzle;
   }
 
   // Получить наиболее вероятное число
-  // Получение секции
-  // Фильтрации невыбранных значений по линии
-  // Фильтрации невыбранных значений по колонке
-  // Фильтрация невыбраных значений в секции
+  function getProbableValue(puzzle, lineIndex, columnIndex) {
+    const probableValues = SUDOKU_VALUES.filter(
+      probable(puzzle[lineIndex], getColumn(puzzle, columnIndex), getPuzzleSection(puzzle, lineIndex, columnIndex))
+    );
 
-  sudoku([
+    if (!probableValues.length) {
+      console.error('has not probableValues', puzzle);
+      return 0;
+    }
+
+    return probableValues.length > 1 ? 0 : probableValues[0];
+  }
+
+  /**
+   * Фильтрация невыбранных значений
+   * @param {number[]} line
+   * @param {number[]} column
+   * @param {number[]} section
+   */
+  function probable(line, column, section) {
+    const checked = [...line, ...column, ...section].filter(Boolean);
+    return value => !checked.includes(value);
+  }
+  /**
+   * Получение всей колонки по индексу
+   * @param {number[][]} puzzle
+   * @param {number} columnIndex
+   */
+  function getColumn(puzzle, columnIndex) {
+    return puzzle.reduce((acc, line) => [...acc, line[columnIndex]], []);
+  }
+  /**
+   * Получение секции
+   * @param {number[][]} puzzle
+   * @param {number} lineIndex
+   * @param {number} columnIndex
+   */
+  function getPuzzleSection(puzzle, lineIndex, columnIndex) {
+    const sectionLineIndex = getPuzzleSectionIndex(lineIndex);
+    sectionColumnIndex = getPuzzleSectionIndex(columnIndex);
+    result = [];
+    for (let i = sectionLineIndex; i < sectionLineIndex + SUDOKU_SECTION_DIVIDER; i++) {
+      result.push(...puzzle[i].slice(sectionColumnIndex, sectionColumnIndex + SUDOKU_SECTION_DIVIDER));
+    }
+    return result;
+  }
+  /**
+   * Получение индекса для секции
+   * @param {number} index
+   */
+  function getPuzzleSectionIndex(index) {
+    return Math.max(0, Math.ceil((index + 1) / SUDOKU_SECTION_DIVIDER) - 1) * SUDOKU_SECTION_DIVIDER;
+  }
+
+  const sudokuValue = sudoku([
     [5, 3, 0, 0, 7, 0, 0, 0, 0],
     [6, 0, 0, 1, 9, 5, 0, 0, 0],
     [0, 9, 8, 0, 0, 0, 0, 6, 0],
@@ -42,4 +78,21 @@ window.onload = () => {
     [0, 0, 0, 4, 1, 9, 0, 0, 5],
     [0, 0, 0, 0, 8, 0, 0, 7, 9]
   ]);
+
+  console.log(
+    '---',
+    'sudoku result',
+    JSON.stringify(sudokuValue) ===
+      JSON.stringify([
+        [5, 3, 4, 6, 7, 8, 9, 1, 2],
+        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+        [8, 5, 9, 7, 6, 1, 4, 2, 3],
+        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+        [3, 4, 5, 2, 8, 6, 1, 7, 9]
+      ])
+  );
 };
